01aef14a380cc64211b66a78d11c9ade3b4c277c
ovalves <vinicius_o.a@live.com>
2021-11-25 11:22:26 -0300

    SPV-ETL

--- mysql.py
+++ mysql.py
@@ -13,22 +13,21 @@ from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv
 
-class dbConnection:
-    def __init__(self):
-        load_dotenv()
+load_dotenv()
+HOST     = os.getenv('DB_HOST')
+PORT     = os.getenv('DB_PORT')
+USER     = os.getenv('DB_USERNAME')
+PASSWORD = os.getenv('DB_PASSWORD')
+DATABASE = os.getenv('DB_DATABASE')
 
-        self.HOST     = os.getenv('DB_HOST')
-        self.PORT     = os.getenv('DB_PORT')
-        self.USER     = os.getenv('DB_USERNAME')
-        self.PASSWORD = os.getenv('DB_PASSWORD')
-        self.DATABASE = os.getenv('DB_DATABASE')
+class dbConnection:
 
     def getConnection():
         return pymysql.connect(
-            host       = dbConnection.HOST,
-            port       = dbConnection.PORT,
-            user       = dbConnection.USER,
-            password   = dbConnection.PASSWORD,
+            host       = HOST,
+            port       = PORT,
+            user       = USER,
+            password   = PASSWORD,
             autocommit = True
         )
 
@@ -67,19 +66,19 @@ class dbConnection:
             return filesList
 
     def insertRawText(self, insertDataframe):
-        engine = create_engine('mysql+pymysql://'+ dbConnection.USER +':'+ dbConnection.PASSWORD +'@'+ dbConnection.HOST +'/'+ dbConnection.DATABASE)
+        engine = create_engine('mysql+pymysql://'+ USER +':'+ PASSWORD +'@'+ HOST +'/'+ DATABASE)
         insertDataframe.columns = ['fileId', 'rawText']
         insertDataframe.set_index('fileId', inplace=True)
         insertDataframe.to_sql('ingestion_files', con = engine, if_exists='append')
 
     def insertRefinedText(self, insertDataframe):
-        engine = create_engine('mysql+pymysql://'+ dbConnection.USER +':'+ dbConnection.PASSWORD +'@'+ dbConnection.HOST +'/'+ dbConnection.DATABASE)
+        engine = create_engine('mysql+pymysql://'+ USER +':'+ PASSWORD +'@'+ HOST +'/'+ DATABASE)
         insertDataframe.columns = ['fileId', 'refinedText']
         insertDataframe.set_index('fileId', inplace=True)
         insertDataframe.to_sql('refined_files', con = engine, if_exists='append')
 
     def insertFeatureText(self, insertDataframe):
-        engine = create_engine('mysql+pymysql://'+ dbConnection.USER +':'+ dbConnection.PASSWORD +'@'+ dbConnection.HOST +'/'+ dbConnection.DATABASE)
+        engine = create_engine('mysql+pymysql://'+ USER +':'+ PASSWORD +'@'+ HOST +'/'+ DATABASE)
         insertDataframe.columns = ['fileId', 'word', 'sentence']
         insertDataframe.set_index('fileId', inplace=True)
         insertDataframe.to_sql('feature_files', con = engine, if_exists='append')
